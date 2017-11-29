dae3681e48cf42f6cb72cce1e6c1761c8175311c
Mohammad MDSA <samimd.77@gmail.com>
2017-11-29 22:06:07 +0330

    Added Service class

---
+++ DSProject0/Model/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSProject0.Model
+{
+	class Service
+	{
+		public string ServiceName { get; }
+		public string CustomerDescription { get; }
+		public string TechnicalDescription { get; }
+		public string CarModel { get; }
+		public int Expence { get; }
+	}
+}
