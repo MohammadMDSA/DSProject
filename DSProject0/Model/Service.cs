b85bb50b0e1788efd21f0838684eaba033ba3fb4
Mohammad MDSA <samimd.77@gmail.com>
2017-11-30 13:07:59 +0330

    Added LinkedList class

--- DSProject0/Model/Service.cs
+++ DSProject0/Model/Service.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSProject0.Util;
 
 namespace DSProject0.Model
 {
@@ -13,10 +13,22 @@ namespace DSProject0.Model
 		public string TechnicalDescription { get; }
 		public string CarModel { get; }
 		public int Expence { get; }
+		private LinkedList<SubService> SubServices;
+
+		Service(string name, string cusDesc, string techDesc, string carModel, int expence)
+		{
+			this.Name = name;
+			this.CustomerDescription = cusDesc;
+			this.TechnicalDescription = techDesc;
+			this.CarModel = carModel;
+			this.Expence = expence;
+		}
 	}
 
 	class SubService
 	{
 		public string Name { get; }
+		private LinkedList<SubService> SubServices;
+		private LinkedList<Service> ParrentServices;
 	}
 }
---
+++ DSProject0/Util/LinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace DSProject0.Util
+{
+	public class LinkedList<T>
+	{
+		public int Length { get; private set; }
+		private LinkedListNode<T> _FirstNode;
+		private LinkedListNode<T> _LastNode;
+
+		public LinkedList()
+		{
+			_FirstNode = _LastNode = null;
+			Length = 0;
+		}
+
+		public LinkedList<T> AddLast(T data)
+		{
+			var newNode = new LinkedListNode<T>();
+			newNode.Data = data;
+			newNode.PreviousNode = _LastNode;
+			_LastNode = newNode;
+			if (this.Length == 0)
+				_FirstNode = newNode;
+			Length++;
+
+			return this;
+		}
+
+		public LinkedListNode<T> FirstNode
+		{
+			get { if (Length == 0) throw new EmptyLinkedListException("The arraylist is empty"); else return _FirstNode; }
+		}
+		public LinkedListNode<T> LastNode
+		{
+			get { if (Length == 0) throw new EmptyLinkedListException("The arraylist is empty"); else return _LastNode; }
+		}
+
+	}
+
+	public class EmptyLinkedListException : Exception
+	{
+		public EmptyLinkedListException(string message) : base(message) { }
+	}
+
+	public sealed class LinkedListNode<V>
+	{
+		public V Data { get; set; }
+		public LinkedListNode<V> NextNode { get; set; }
+		public LinkedListNode<V> PreviousNode { get; set; }
+
+		public LinkedListNode(V data) : this()
+		{
+			this.Data = data;
+		}
+
+		public LinkedListNode()
+		{
+			this.NextNode = null;
+			this.PreviousNode = null;
+		}
+	}
+}
